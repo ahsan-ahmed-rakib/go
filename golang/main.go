9cff7c52fc27152d3d0b8805a2cdb4bccaaf88b9
Ahsan Ahmed Rakib <ahsan.rakib@technonext.com>
2026-04-19 10:32:36 +0600

    For loop

--- golang/main.go
+++ golang/main.go
@@ -3,10 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	test()
-	fmt.Println("Hello GO!")
-}
+	fmt.Println("Welcome to our Todolist App!")
 
-func test() {
-	fmt.Println("Test")
+	var taskItems = []string {"Watch go tutorial", "Do go code after see video", "Build API with go"}
+
+	for index, task := range taskItems {
+		// fmt.Println(index + 1,":", task)
+		fmt.Printf("%d: %s\n", index+1, task)
+	}
+
+	// fmt.Println("Tasks: ", taskItems)
 }
