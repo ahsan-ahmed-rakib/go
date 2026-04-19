8207ca0ceb842c6a513db2f32e6f85f2d527fb5c
Ahsan Ahmed Rakib <ahsan.rakib@technonext.com>
2026-04-19 12:23:36 +0600

    Diff functions

--- golang/main.go
+++ golang/main.go
@@ -3,14 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our Todolist App!")
-
+	
 	var taskItems = []string {"Watch go tutorial", "Do go code after see video", "Build API with go"}
+	println("This is main function")
 
+	// printTexts(taskItems)
+	addTask(taskItems, "Learn GO function")
+}
+
+func printTexts(taskItems []string) {
+	fmt.Println("Welcome to our Todolist App!")
 	for index, task := range taskItems {
-		// fmt.Println(index + 1,":", task)
 		fmt.Printf("%d: %s\n", index+1, task)
 	}
+}
 
-	// fmt.Println("Tasks: ", taskItems)
+func addTask(taskItems []string, newTask string) {
+	var updatedTasks = append(taskItems, newTask)
+	printTexts(updatedTasks)
 }
